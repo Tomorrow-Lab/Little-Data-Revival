6b5fcd281cc405f42ea069f3279b99b0853917aa
Tomorrow-Lab <adrianspearl@gmail.com>
2017-02-28 15:06:00 -0500

    Added text input feature

--- jqueryexample.py
+++ jqueryexample.py
@@ -9,20 +9,29 @@
     :license: BSD, see LICENSE for more details.
 """
 from flask import Flask, jsonify, render_template, request
+import serial, time
+ser = serial.Serial('/dev/ttyACM0',9600)
 app = Flask(__name__)
 
+@app.route('/_push_text')
+def push_text():
+    t = request.args.get('t', 0, type=str)
+    ser.write(t.encode())
+    ser.write('#'.encode())
+    return jsonify(result=a + b + c)
 
 @app.route('/_add_numbers')
 def add_numbers():
     """Add two numbers server side, ridiculous but well..."""
     a = request.args.get('a', 0, type=int)
     b = request.args.get('b', 0, type=int)
-    return jsonify(result=a + b)
-
+    c = request.args.get('c', 0, type=int)
+    ser.write(bytes([a,b,c]))
+    return jsonify(result=a + b + c)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
